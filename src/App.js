b9f80c4978a64490cfc0f0e5ecf5043f634446dc
Moon Chu <mmoonchu2@gmail.com>
2023-12-08 14:02:01 -0500

    Refactored collectSpeakingCharacters function, reduced number of movies used to 1 to temporarily circumvent bug where creation of speakingCharacters array is not behaving as expected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import { Route, Routes, } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Main from "./pages/Main/Main";
 import About from "./pages/About/About";
 import Play from "./pages/Play/Play";
 import './style.css';
 
 function App() {
+  let speakingCharacters = [];
+  const apiKey = process.env.REACT_APP_KEY;
+
   const createLordleKey = function() {
 
   }
 
-  const apiKey = process.env.REACT_APP_KEY;
   // const moviesUrl = `https://the-one-api.dev/v2/movie`;
   const movie1Url = `https://the-one-api.dev/v2/movie/5cd95395de30eff6ebccde5c/quote`;
   const movie2Url = `https://the-one-api.dev/v2/movie/5cd95395de30eff6ebccde5b/quote?limit=1100`;
   const movie3Url = `https://the-one-api.dev/v2/movie/5cd95395de30eff6ebccde5d/quote`;
 
-  let speakingCharacters = [];
-  const convertJSONToArray = function(JSONData) {
-    speakingCharacters.push(...new Set(JSONData.docs.map(quote => quote.character)));
-  }
-  const collectSpeakingCharacters = async(url) => {
+  const fetchData = async(url) => {
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -28,26 +26,20 @@ function App() {
       }
     });
     const data = await response.json();
+    return data;
+  }
+  const collectSpeakingCharacters = async(movieQuotesUrl) => {
+    function convertJSONToArray(JSONData) {
+      speakingCharacters.push(...new Set(JSONData.docs.map(quote => quote.character)));
+    }
+    const data = await fetchData(movieQuotesUrl);
     convertJSONToArray(data);
     speakingCharacters = [...new Set(speakingCharacters)];
-    return speakingCharacters
-  }
-  // collectSpeakingCharacters(movie1Url);
-  // collectSpeakingCharacters(movie2Url);
-  // collectSpeakingCharacters(movie3Url);
-  // console.log(speakingCharacters)
-  const yeah = async() => {
-    const testresult = await collectSpeakingCharacters(movie1Url);
-    const response = await fetch(`https://the-one-api.dev/v2/character/${testresult[12]}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-      }
-    });
-    const data = await response.json();
-    console.log(data.docs[0].name)
-  }
-  yeah();
+    return speakingCharacters;
+  }  
+  useEffect(() => {
+     collectSpeakingCharacters(movie1Url);
+  }, []);
 
   const [lordleKey, setLordleKey] = useState(null);
 
