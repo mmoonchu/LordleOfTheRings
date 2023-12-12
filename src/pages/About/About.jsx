21a1c97d523faaf8d5f5536bed471e477c6b3a32
Moon Chu <mmoonchu2@gmail.com>
2023-12-12 14:48:06 -0500

    Added About.jsx page blurb

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,8 @@ function About() {
   return (
     <div>
       <Link to='/'>Back to Main Menu</Link><br/>
-      About
+      <h1 className='about h1'>Lordle of the Rings</h1>
+      <h6 className='about'>A Wordle-inspired "Lord of the Rings"-themed game where you must try and guess the secret character from Tolkien's Legendarium in as few tries as possible</h6>
     </div>
   )
 }
