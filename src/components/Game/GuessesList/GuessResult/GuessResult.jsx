0ba459df53682404e961c4f9509aaeef7bbc447a
Moon Chu <mmoonchu2@gmail.com>
2023-12-12 01:08:11 -0500

    Created var for guess's character data

--- src/components/Game/GuessesList/GuessResult/GuessResult.jsx
+++ src/components/Game/GuessesList/GuessResult/GuessResult.jsx
@@ -3,18 +3,24 @@ import Square from './Square/Square';
 import { characterDataContext, lordleKeyContext } from '../../../../App';
 
 function GuessResult(props) {
-  //class: 'square {color}'
-  //text: 'propertykey'
   const lordleKey = useContext(lordleKeyContext);
   const characterData = useContext(characterDataContext);
   const [squares, setSquares] = useState([]); // each square will hold color & property of 'this' particular GuessResult
+  const currentGuessData = characterData.docs.find((object) => object.name == lordleKey.name);
+  console.log(currentGuessData)
 
   class Square {
-    constructor(color, propertyName) {
-      this.color = color;
+    constructor(propertyName, color) {
       this.propertyName = propertyName;
+      this.color = color;
     }
   }
+  
+  // const nameSquare = new Square(characterName, red);
+  // const raceSquare = new Square(race, red);
+  // const realmSquare = new Square(realm, red);
+  // const genderSquare = new Square(gender, red);
+  // const heightSquare = new Square(height, red);
 
   return (
     <div>
