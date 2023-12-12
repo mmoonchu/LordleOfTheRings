eda0a4750f308b9f29efbb294a9bfe7927c7ca8d
Moon Chu <mmoonchu2@gmail.com>
2023-12-12 09:12:14 -0500

    Fixed bug where GuessResult.jsx's useEffect would trigger twice (moved code into useEffect)

--- src/components/Game/GuessesList/GuessResult/GuessResult.jsx
+++ src/components/Game/GuessesList/GuessResult/GuessResult.jsx
@@ -7,25 +7,20 @@ function GuessResult(props) {
   const lordleKey = useContext(lordleKeyContext);
   const characterData = useContext(characterDataContext);
   const [squares, setSquares] = useState([]); // each square will hold color & property of 'this' particular GuessResult
-  const currentGuessData = characterData.docs.find((object) => object.name == props.guess);
 
-  class SquareObject {
-    constructor(propertyName, color) {
-      this.propertyName = propertyName;
-      this.classes = `square ${color}`;
-    }
-  }
+  useEffect(() => {
+    const currentGuessData = characterData.docs.find((object) => object.name === props.guess);
 
-  const currentCharacter = {
-    name: currentGuessData.name,
-    race: currentGuessData.race,
-    realm: currentGuessData.realm !== '' ? currentGuessData.realm : 'N/A',
-    gender: currentGuessData.gender,
-    height: currentGuessData.height !== '' ? currentGuessData.height : 'N/A',
-  };
+    const currentCharacter = {
+      name: currentGuessData.name,
+      race: currentGuessData.race,
+      realm: currentGuessData.realm !== '' ? currentGuessData.realm : 'N/A',
+      gender: currentGuessData.gender,
+      height: currentGuessData.height !== '' ? currentGuessData.height : 'N/A',
+    };
 
-  const newArray = [];
-  const createSquares = function() {
+    
+    const newArray = [];
     for (const property in currentCharacter) {
       const propertyValue = currentCharacter[property];
       let propertyColor;
@@ -37,13 +32,11 @@ function GuessResult(props) {
       } else {
         propertyColor = 'red';
       }
-      newArray.push(new SquareObject(propertyValue, propertyColor));
+      newArray.push({ propertyName: propertyValue, classes: `square ${propertyColor}` });
     }
+
     setSquares(newArray);
-  }
-  useEffect(() => {
-    createSquares();
-  }, [])
+  }, [lordleKey])
 
   const guessClass = `guess-result ${props.guess}`
 
