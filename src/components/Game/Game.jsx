c46d75363f93aa229d54c6bc78546a3d5cb685cc
Moon Chu <mmoonchu2@gmail.com>
2023-12-12 03:07:41 -0500

    Centered Game.jsx components

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Input from "./Input/Input";
 import GuessesList from "./GuessesList/GuessesList";
+import './Game.css';
 
 export const guessesContext = React.createContext();
 
