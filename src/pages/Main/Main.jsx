ad3226140561269b267b53e6ef323ee8bd033363
Moon Chu <mmoonchu2@gmail.com>
2023-12-12 14:49:54 -0500

    Added title to Main.jsx page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,6 +5,7 @@ import './Main.css'
 function Main() {
   return (
     <div className='main-menu'>
+      <header className='h1'>Lordle of the Rings</header>
       <Link to='/Play'>Play</Link>
       <Link to='/About'>About</Link>
     </div>
